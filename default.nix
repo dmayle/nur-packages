84c23287f94e251a948be8866056e0370389054b
Douglas Mayle <douglas@mayle.org>
2021-03-18 17:10:59 +0100

    Package tbsm instead of ly

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  tbsm = pkgs.callPackage ./pkgs/tbsm { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
